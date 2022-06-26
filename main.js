63704d11fdf1c542b9aa6c08c532f04acdfd3a87
Antonija Simic <antonija1023@gmail.com>
2022-06-26 16:52:28 +0200

    make a starter template for students

--- main.js
+++ main.js
@@ -1,22 +1,2 @@
 import './style.scss';
-
-const createInputs = (i, field) => (`<input type='checkbox' id='checkbox-step${i}' />
-<label for='checkbox-step${i}' class='label-step label-step${i}'>
-  <input type='text' class='input' id='input-step${i}' required placeholder='No placeholder' />
-  <span class='msg'></span>
-  <label for='input-step${i}' class='label'>
-   ${field}
-  </label>
-  <span class='button'>Next</span>
-</label>
-`);
-
-document.querySelector('#app').innerHTML = `
-  <div class="form-container">
-    <form>
-      ${createInputs(1, 'Email')}
-      ${createInputs(2, 'Password')}
-      ${createInputs(3, 'Confirm password')}
-    </form>
-  </div>
-`;
+document.querySelector('#app').innerHTML = `<h1> A multistep form built with pure Scss </h1>`;
